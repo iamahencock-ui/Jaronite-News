35c1ace7bad21cbcf5fe9f11b443b9cdde194fa6
Weroarforareason <iamahen.cock@gmail.com>
2026-06-19 21:20:56 -0700

    changed index.js transience

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
+import manifestJSON from "__STATIC_CONTENT_MANIFEST";
+
+const assetManifest = JSON.parse(manifestJSON);
 
 export default {
   async fetch(request, env, ctx) {
     try {
-      return await getAssetFromKV({ request, waitUntil: ctx.waitUntil.bind(ctx) }, { ASSET_NAMESPACE: env.__STATIC_CONTENT });
+      return await getAssetFromKV(
+        {
+          request,
+          waitUntil(promise) {
+            return ctx.waitUntil(promise);
+          },
+        },
+        {
+          ASSET_NAMESPACE: env.__STATIC_CONTENT,
+          ASSET_MANIFEST: assetManifest,
+        }
+      );
     } catch (e) {
       return new Response("404 Not Found", { status: 404 });
     }
-  }
+  },
 };
